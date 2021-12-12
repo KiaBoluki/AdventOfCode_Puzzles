e4fb461c005cb07e54d79979cea8ad0445a359c4
Kia Boluki <Kia.Boluki@gmail.com>
2021-12-13 00:13:50 +0330

    Part Three Completed

--- main.py
+++ main.py
@@ -1,54 +1,25 @@
 def main() : 
-  report = read_file()
-  
-  # to get the result of part 1 uncomment the following line
-  #get_sum_of_increases(report)
+  report = read_file('submarine_move_plan.txt')
+  get_final_position(report)
   
   
-  get_sum_of_sliding_window(report)  
-  
-def read_file():
-  with open('./submarine_report.txt', 'r') as submarine_report:
+def read_file(file_name):
+  with open(f'./{file_name}', 'r') as submarine_report:
     report = submarine_report.readlines()
     return report
 
-def get_sum_of_increases(report):
-  prev_number = int(report[0])
-  increase_counter = 0 
-  decrease_counter = 0
-  for index,deep_num in enumerate(report):
-    
-    current_num = int(deep_num)
-    diff = current_num - prev_number
-    if diff > 0 :
-      increase_counter += 1
-      # print(f'{index}:\r\n{prev_number}\r\n{current_num}\r\n+Increase\r\n')
-    else :
-      decrease_counter += 1
-      # print(f'{index}:\r\n{prev_number}\r\n{current_num}\r\n-Decrease\r\n')
-    prev_number = current_num
-    
-  print(f'Increase:{increase_counter}',f'Decrease:{decrease_counter}')
+def get_final_position(report):
+  forward = 0 
+  deep = 0 
+  for line in report:
+    splited_line = line.split()
+    if (splited_line[0] == 'forward'):
+      forward += int(splited_line[1])
+    elif (splited_line[0] == 'down'):
+      deep += int(splited_line[1])
+    elif (splited_line[0] == 'up'):
+      deep -= int(splited_line[1])
+  print(f'Final position: {int(forward*deep)}')
   
   
-def get_sum_of_sliding_window(report):
-  increase_counter = 0
-  decrease_counter = 0
-  for index in range(len(report)-len(report)%3):
-    slide_1 = report[index:index+3]
-    slide_2 = report[index+1:index+4]
-    sum_1 = sum(map(int,slide_1))
-    sum_2 = sum(map(int,slide_2))
-    diff = sum_2 - sum_1
-    if diff > 0 :
-      increase_counter += 1
-      
-    else :
-      decrease_counter += 1
-      
-  print('Increase:',increase_counter,'Decrease:',decrease_counter)
-
-
-
-
 main()
